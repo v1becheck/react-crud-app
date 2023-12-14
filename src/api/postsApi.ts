52eff15217218d1aedf9d524be8ce8047e961aa7
Milos Ilic <milosilic89@outlook.com>
2023-12-15 00:58:28 +0100

    Added node/types support

--- src/api/postsApi.ts
+++ src/api/postsApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_BASE_URL;
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 export const fetchPosts = () => axios.get(API_BASE);
 export const fetchPost = (id: number) => axios.get(`${API_BASE}/${id}`);
